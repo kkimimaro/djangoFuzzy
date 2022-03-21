a1b6ffbb096a02b8565bfe7564802f029d686f90
dannu <dannu2001@gmail.com>
2022-03-21 22:39:53 +0300

    first commit

--- djangoFuzzievich/settings.py
+++ djangoFuzzievich/settings.py
@@ -123,3 +123,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
